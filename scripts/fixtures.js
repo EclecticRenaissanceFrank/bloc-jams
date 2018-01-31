c3c5f31841f1fc81c666bb10e57566144386e4b4
Frank Mooradian <EclecticRenaissanceFrank@Gmail com>
2018-01-30 22:03:43 -0500

    checkpoint-32-jQuery-Next-and-Previous-Button complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,49 @@
+
+// Example Album
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Blue', duration: '4:26' },
+    { title: 'Green', duration: '3:14' },
+    { title: 'Red', duration: '5:01' },
+    { title: 'Pink', duration: '3:21'},
+    { title: 'Magenta', duration: '2:15'}
+  ]
+};
+
+
+// Another Example Album
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21'},
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+
+
+var albumStrungOut = {
+  title: 'Twisted By Design',
+  artist: 'Strung Out',
+  label: 'Fat Wreck Chords',
+  year: '1998',
+  albumArtUrl: 'assets/images/album_covers/10.png',
+  songs: [
+    { title: 'Too Close To See?', duration: '3:01' },
+    { title: 'Mind Of My Own', duration: '4:01' },
+    { title: 'Matchbook', duration: '4:21'},
+    { title: 'Ice Burn', duration: '2:14' },
+    { title: 'Wrong Side Of The Tracks', duration: '2:45'}
+  ]
+};
