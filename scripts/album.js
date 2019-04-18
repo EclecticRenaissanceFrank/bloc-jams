dcfdc95105dbd0c98826c2b5b3e52cb2cb836a49
Frank Mooradian <EclecticRenaissanceFrank@Gmail com>
2019-04-18 19:34:36 -0400

    Directions Added

--- scripts/album.js
+++ scripts/album.js
@@ -367,3 +367,29 @@ var previousSong = function() {
     $previousSongNumberCell.html(pauseButtonTemplate);
     $lastSongNumberCell.html(lastSongNumber);
 };
+
+
+
+
+// Write a function called setCurrentTimeInPlayerBar() that takes one argument, currentTime, that sets the text of the element with the .current-time class to the current time in the song.
+// Add the method to updateSeekBarWhileSongPlays() so the current time updates with song playback.
+
+
+
+// Write a function called setTotalTimeInPlayerBar() that takes one argument, totalTime, that sets the text of the element with the .total-time class to the length of the song.
+// Add the method to updatePlayerBarSong() so the total time is set when a song first plays.
+
+
+
+// Write a function called filterTimeCode that takes one argument, timeInSeconds. It should:
+// Use the parseFloat() method to get the seconds in number form.
+// Store variables for whole seconds and whole minutes (hint: use Math.floor() to round numbers down).
+// Return the time in the format X:XX
+
+
+
+// Wrap the arguments passed to setCurrentTimeInPlayerBar() and setTotalTimeInPlayerBar() in a filterTimeCode() call so the time output below the seek bar is formatted.
+
+
+
+// Wrap the songLength variable in createSongRow() in a filterTimeCode() call so the time lengths are formatted.
